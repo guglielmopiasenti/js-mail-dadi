ac3d73908946ca673d8ce67d9b1cc0ed5722c4df
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-05-31 18:38:05 +0200

    creating event listener

--- mail.js
+++ mail.js
@@ -46,25 +46,34 @@ login.addEventListener('click', function () {
 
 // Dice Exercise
 
-// Generate a random number from 1 to 6 for the player
-let player = Math.floor(Math.random() * 6) + 1;
+// Get Button element from the DOM
+playButton = document.getElementById('playButton');
+
+// creating event listener for the button
+playButton.addEventListener('click', function () {
+
+    // Generate a random number from 1 to 6 for the player
+    let player = Math.floor(Math.random() * 6) + 1;
+    
+    
+    // Generate a random number from 1 to 6 for the CPU
+    let computer = Math.floor(Math.random() * 6) + 1;
+    
+    // Determine the winner based on the highest score
+    let winner;
+    if (player > computer) {
+        winner = player;
+    } else if (player < computer) {
+        winner = computer;
+    } else {
+        winner = "It's a tie!";
+    }
+    
+    
+    // Print the result in the console
+    console.log("Player: " + player);
+    console.log("Computer: " + computer);
+    console.log("Winner: " + winner);
 
 
-// Generate a random number from 1 to 6 for the CPU
-let computer = Math.floor(Math.random() * 6) + 1;
-
-// Determine the winner based on the highest score
-let winner;
-if (player > computer) {
-    winner = player;
-} else if (player < computer) {
-    winner = computer;
-} else {
-    winner = "It's a tie!";
-}
-
-
-// Print the result in the console
-console.log("Player: " + player);
-console.log("Computer: " + computer);
-console.log("Winner: " + winner);
+});
