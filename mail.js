cb191dc9c5759fb09ab5ae1acb32ecf35d4aca1a
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-05-31 15:04:09 +0200

    creating email list

---
+++ mail.js
@@ -0,0 +1,13 @@
+//  Create a list of emails (generated automatically)
+email_list = [
+    'john.doe@example.com',
+    'jane.doe@example.com',
+    'alex.smith@example.com',
+    'emma.wilson@example.com',
+    'michael.brown@example.com',
+    'sarah.johnson@example.com',
+    'robert.williams@example.com',
+    'emily.jones@example.com',
+    'david.davis@example.com',
+    'olivia.brown@example.com'
+]
